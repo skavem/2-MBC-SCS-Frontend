c1c701e3dbe5097ff1e2d17e805e7454bdde5385
Skavem <skavem@gmail.com>
2022-09-24 20:12:03 +0300

    visual: loading between songs and Bible is quick now, but lists are loaded with LoadingSpinner shown

--- src/components/items-list/ItemsList.tsx
+++ src/components/items-list/ItemsList.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { ISObjectWRKey } from "../../models";
 import { IItemButton, IListButton } from "../../models/ui";
 import concatClasses from "../../utils/concatClasses";
 import isScrollNeeded from "../../utils/isScrollNeeded";
+import LoadingSpinner from "../LoadingSpinner";
 import Item from "./Item";
 import styles from "./items-list.module.css";
 
@@ -34,6 +35,7 @@ const ItemsList = ({
 }: IItemsListProps) => {
   const activeRef = useRef<HTMLDivElement | null>(null);
   const list = useRef<HTMLDivElement | null>(null);
+  const [firstLoad, setFirstLoad] = useState(() => true)
 
   useEffect(() => {
     if (activeRef.current === null || list.current === null) return;
@@ -42,60 +44,72 @@ const ItemsList = ({
     }
   }, [activeItem]);
 
+  useEffect(() => {
+    setFirstLoad(false)
+  }, [])
+
   return (
     <div ref={list} className={concatClasses(styles.ItemsList, className)}>
-      {listTopButton && (
-        <Item.Button
-          className={listTopButton.className}
-          name={listTopButton.name}
-          onClick={listTopButton.onClick}
-        >
-          {listTopButton.children}
-        </Item.Button>
-      )}
-      {items?.map((item) => {
-        const isActive = item.reactKey === activeItem?.reactKey;
-        return (
-          <Item
-            className={itemClassName}
-            item={item}
-            forwardRef={isActive ? activeRef : null}
-            isActive={isActive}
-            isShown={isItemShown}
-            onClick={onItemClick}
-            onDoubleClick={onDoubleItemClick}
-            key={item.reactKey}
+      {!firstLoad ? 
+      <>
+        {listTopButton && (
+          <Item.Button
+            className={listTopButton.className}
+            name={listTopButton.name}
+            onClick={listTopButton.onClick}
           >
-            <div className={styles['ItemsList-Item-RightButtons']}>
-              {rightButtons?.map((button) => (
-                <Item.Button
-                  name={button.name}
-                  onClick={() => button.onClick(item)}
-                  className={concatClasses(
-                    button.className,
-                    (isActive ? button.activeClassName : button.nonActiveClassName)
-                  )}
-                  key={`${item.reactKey}-${button.name as string}`}
-                >
-                  {button.children}
-                </Item.Button>
-              ))}
-            </div>
-            <span className={styles['ItemList-Item-BottomButtons']}>
-              {bottomButtons?.map((button) => (
-                <Item.Button
-                  name={button.name}
-                  onClick={() => button.onClick(item)}
-                  className={concatClasses(button.className!)}
-                  key={`${item.reactKey}-${button.name as string}`}
-                >
-                  {button.children}
-                </Item.Button>
-              ))}
-            </span>
-          </Item>
-        );
-      })}
+            {listTopButton.children}
+          </Item.Button>
+        )}
+
+        {items?.map((item) => {
+          const isActive = item.reactKey === activeItem?.reactKey;
+          return (
+            <Item
+              className={itemClassName}
+              item={item}
+              forwardRef={isActive ? activeRef : null}
+              isActive={isActive}
+              isShown={isItemShown}
+              onClick={onItemClick}
+              onDoubleClick={onDoubleItemClick}
+              key={item.reactKey}
+            >
+              <div className={styles['ItemsList-Item-RightButtons']}>
+                {rightButtons?.map((button) => (
+                  <Item.Button
+                    name={button.name}
+                    onClick={() => button.onClick(item)}
+                    className={concatClasses(
+                      button.className,
+                      (isActive ? button.activeClassName : button.nonActiveClassName)
+                    )}
+                    key={`${item.reactKey}-${button.name as string}`}
+                  >
+                    {button.children}
+                  </Item.Button>
+                ))}
+              </div>
+              <span className={styles['ItemList-Item-BottomButtons']}>
+                {bottomButtons?.map((button) => (
+                  <Item.Button
+                    name={button.name}
+                    onClick={() => button.onClick(item)}
+                    className={concatClasses(button.className!)}
+                    key={`${item.reactKey}-${button.name as string}`}
+                  >
+                    {button.children}
+                  </Item.Button>
+                ))}
+              </span>
+            </Item>
+          );
+        })}
+      </> : 
+      <div className={styles['ItemsList-LoadingContainer']}>
+        <LoadingSpinner className="h-16" />
+      </div>
+      }
     </div>
   );
 };
